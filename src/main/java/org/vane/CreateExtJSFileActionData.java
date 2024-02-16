fab78deb6f0eb88b4221589c0d0c57e13379042d
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2024-02-16 14:42:51 +0900

    move const variable

---
+++ src/main/java/org/vane/CreateExtJSFileActionData.java
@@ -0,0 +1,18 @@
+package org.vane;
+
+interface CreateExtJSFileActionData {
+    // templates file name
+    String MAIN = "Main";
+    String CONTROLLER = "Controller";
+    String VIEW_MODEL = "ViewModel";
+    // file name
+    String FILE_CONTROLLER = "ViewController";
+    String FILE_MODEL = "ViewModel";
+    // properties
+    String ALIAS = "ALIAS";
+    String DEFAULT_LAYOUT = "DEFAULT_LAYOUT";
+    String HEIGHT = "HEIGHT";
+    String WIDTH = "WIDTH";
+
+    String TYPE = "TYPE";
+}
