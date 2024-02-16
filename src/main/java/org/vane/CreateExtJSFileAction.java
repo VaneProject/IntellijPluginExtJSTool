81f4e3ee9dda6c85ac66f5b70b1f4591db2eee92
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2024-02-16 15:18:40 +0900

    create const name

--- src/main/java/org/vane/CreateExtJSFileAction.java
+++ src/main/java/org/vane/CreateExtJSFileAction.java
@@ -1,6 +1,5 @@
 package org.vane;
 
-import com.intellij.codeInsight.navigation.BackgroundUpdaterTask;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
@@ -13,10 +12,12 @@ import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.vane.setting.ExtJSToolSetting;
 
 import java.util.Properties;
 
-public class CreateExtJSFileAction extends CreateFileFromTemplateAction {
+public class CreateExtJSFileAction extends CreateFileFromTemplateAction implements CreateExtJSFileActionData {
+
     protected CreateExtJSFileAction() {
         super("Create ExtJS Files", "Create extJS files", null);
     }
@@ -27,7 +28,9 @@ public class CreateExtJSFileAction extends CreateFileFromTemplateAction {
                                @NotNull CreateFileFromTemplateDialog.Builder builder) {
         builder
                 .setTitle("Create ExtJS Tool")
-                .addKind("ExtJS", AllIcons.FileTypes.JavaScript, "Main.js");
+                .addKind("ExtJS", AllIcons.FileTypes.JavaScript, MAIN)
+                .addKind("Controller", AllIcons.FileTypes.JavaScript, CONTROLLER)
+                .addKind("ViewModel", AllIcons.FileTypes.JavaScript, VIEW_MODEL);
     }
 
     @Override
@@ -39,20 +42,52 @@ public class CreateExtJSFileAction extends CreateFileFromTemplateAction {
 
     @Override
     protected PsiFile createFileFromTemplate(String name, FileTemplate template, PsiDirectory dir) {
-        Project project = dir.getProject();
-        FileTemplate controller = FileTemplateManager.getInstance(project).getInternalTemplate("Controller.js");
-        FileTemplate viewModel = FileTemplateManager.getInstance(project).getInternalTemplate("ViewModel.js");
-        try {
-            Properties properties = FileTemplateManager.getInstance(project).getDefaultProperties();
-            properties.setProperty("ALIAS", toAlias(name));
+        return switch (template.getName()) {
+            case MAIN -> {
+                Project project = dir.getProject();
+                FileTemplate controller = FileTemplateManager.getInstance(project).getInternalTemplate(CONTROLLER);
+                FileTemplate viewModel = FileTemplateManager.getInstance(project).getInternalTemplate(VIEW_MODEL);
+                // properties
+                Properties properties = getProperties(project, name);
+                ExtJSToolSetting setting = ExtJSToolSetting.getInstance();
+                try {
+                    // create template
+                    FileTemplateUtil.createFromTemplate(controller, name + setting.controllerName, properties, dir);
+                    FileTemplateUtil.createFromTemplate(viewModel, name + FILE_MODEL, properties, dir);
+                    yield FileTemplateUtil.createFromTemplate(template, name, properties, dir).getContainingFile();
+                } catch (Exception e) {
+                    LOG.error(e);
+                }
+                yield null;
+            }
+            case VIEW_MODEL, CONTROLLER -> {
+                Project project = dir.getProject();
+                // properties
+                Properties properties = getProperties(project, name);
+                try {
+                    yield FileTemplateUtil.createFromTemplate(template, name, properties, dir).getContainingFile();
+                } catch (Exception e) {
+                    LOG.error(e);
+                }
+                yield null;
+            }
+            default -> super.createFileFromTemplate(name, template, dir);
+        };
+    }
 
-            FileTemplateUtil.createFromTemplate(controller, name + "Controller", properties, dir);
-            FileTemplateUtil.createFromTemplate(viewModel, name + "ViewModel", properties, dir);
-
-            return FileTemplateUtil.createFromTemplate(template, name, properties, dir).getContainingFile();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    private Properties getProperties(Project project, String name) {
+        Properties properties = FileTemplateManager.getInstance(project).getDefaultProperties();
+        properties.setProperty(ALIAS, toAlias(name));
+        // setting value
+        ExtJSToolSetting setting = ExtJSToolSetting.getInstance();
+        properties.setProperty(DEFAULT_LAYOUT, setting.defaultLayout);
+        properties.setProperty(HEIGHT, setting.height);
+        properties.setProperty(WIDTH, setting.width);
+        // model
+        properties.setProperty(TYPE, setting.type);
+        // controller
+        properties.setProperty(CONTROLLER_NAME, setting.controllerName);
+        return properties;
     }
 
     private String toAlias(String name) {
