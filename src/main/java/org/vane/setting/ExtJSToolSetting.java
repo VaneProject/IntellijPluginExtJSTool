13f17b65fddb512dccafc202a1f7c96a894c66c6
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2024-02-17 11:17:20 +0900

    add property project name

--- src/main/java/org/vane/setting/ExtJSToolSetting.java
+++ src/main/java/org/vane/setting/ExtJSToolSetting.java
@@ -1,5 +1,7 @@
 package org.vane.setting;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
@@ -7,11 +9,16 @@ import com.intellij.openapi.components.Storage;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.*;
+
 @State(
         name = "org.vane.setting.ExtJSToolSetting",
         storages = @Storage("ExtJSToolSettingsPlugin.xml")
 )
 public final class ExtJSToolSetting implements PersistentStateComponent<ExtJSToolSetting> {
+    public String projectName = "Ext";
+    public String appJsonPath = "";
+    public boolean appJsonPathCheck = false;
     // view
     public String defaultLayout = "border";
     public String height = "720", width = "1080";
@@ -32,5 +39,18 @@ public final class ExtJSToolSetting implements PersistentStateComponent<ExtJSToo
     @Override
     public void loadState(@NotNull ExtJSToolSetting setting) {
         XmlSerializerUtil.copyBean(setting, this);
+    }
+
+    public String getProjectName() {
+        if (appJsonPathCheck) {
+            File file = new File(appJsonPath);
+            try (Reader reader = new FileReader(file)) {
+                JsonElement json = JsonParser.parseReader(reader);
+                return json.getAsJsonObject().get("name").getAsString();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else
+            return projectName;
     }
 }
--- src/main/java/org/vane/setting/ExtJSToolSettingsComponent.java
+++ src/main/java/org/vane/setting/ExtJSToolSettingsComponent.java
@@ -1,24 +1,74 @@
 package org.vane.setting;
 
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.ui.ComponentValidator;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.ui.ValidationInfo;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.ui.DocumentAdapter;
+import com.intellij.ui.components.JBCheckBox;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBTextField;
 import com.intellij.util.ui.FormBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import java.io.File;
 import java.util.Optional;
 
-public class ExtJSToolSettingsComponent {
+public final class ExtJSToolSettingsComponent implements ExtJSToolSettingsComponentValid {
+    private final JLabel projectNameLabel = new JBLabel("Project name: ");
+    private final JLabel appJsonPathLabel = new JBLabel("File path app.json: ");
+    private final JBTextField projectName = new JBTextField();
+    private final JBCheckBox appJsonPathCheck = new JBCheckBox("Use app.json option: ");
+    private final TextFieldWithBrowseButton appJsonPath = new TextFieldWithBrowseButton();
     private final JPanel mainPanel;
     private final CreateView view;
     private final CreateModel model;
     private final CreateController controller;
 
     public ExtJSToolSettingsComponent(Disposable disposable) {
+        setValidBlank(disposable, projectName);
+        // app.json setting
+        validList.add(appJsonPath);
+        new ComponentValidator(disposable).withValidator(() -> {
+            if (!appJsonPath.isEnabled())
+                return null;
+            String text = appJsonPath.getText();
+            if (StringUtil.isEmptyOrSpaces(text))
+                return new ValidationInfo("Please enter a text", appJsonPath);
+            File file = new File(text);
+            if (!file.isFile())
+                return new ValidationInfo("Do not find file", appJsonPath);
+            return null;
+        }).installOn(appJsonPath);
+        appJsonPath.getTextField().getDocument().addDocumentListener(new DocumentAdapter() {
+            @Override
+            protected void textChanged(@NotNull DocumentEvent documentEvent) {
+                ComponentValidator.getInstance(appJsonPath).ifPresent(ComponentValidator::revalidate);
+            }
+        });
+        appJsonPath.addBrowseFolderListener("Choose File", null, null,
+                new FileChooserDescriptor(true, false, false, false, false, false)
+        );
+        appJsonPathCheck.addChangeListener(e -> {
+            boolean isCheck = ((AbstractButton) e.getSource()).getModel().isSelected();
+            projectName.setEnabled(!isCheck);
+            projectNameLabel.setEnabled(!isCheck);
+            appJsonPath.setEnabled(isCheck);
+            appJsonPathLabel.setEnabled(isCheck);
+        });
+
         view = new CreateView(disposable);
         model = new CreateModel(disposable);
         controller = new CreateController(disposable);
 
         mainPanel = FormBuilder.createFormBuilder()
+                .addComponent(appJsonPathCheck, 1)
+                .addLabeledComponent(projectNameLabel, projectName, 1)
+                .addLabeledComponent(appJsonPathLabel, appJsonPath, 1)
                 .addComponent(view.getPanel())
                 .addComponent(controller.getPanel())
                 .addComponent(model.getPanel())
@@ -26,14 +76,19 @@ public class ExtJSToolSettingsComponent {
                 .getPanel();
     }
 
-    // init method
-    void resetData(ExtJSToolSetting setting) {
+    public void resetData(ExtJSToolSetting setting) {
+        projectName.setText(setting.projectName);
+        appJsonPath.setText(setting.appJsonPath);
+        appJsonPathCheck.setSelected(setting.appJsonPathCheck);
         view.resetData(setting);
         model.resetData(setting);
         controller.resetData(setting);
     }
 
-    void applyData(ExtJSToolSetting setting) {
+    public void applyData(ExtJSToolSetting setting) {
+        setting.projectName = projectName.getText();
+        setting.appJsonPath = appJsonPath.getText();
+        setting.appJsonPathCheck = appJsonPathCheck.isSelected();
         view.applyData(setting);
         model.applyData(setting);
         controller.applyData(setting);
@@ -52,6 +107,18 @@ public class ExtJSToolSettingsComponent {
     // property
     public JPanel getPanel() {
         return mainPanel;
+    }
+
+    public String getProjectName() {
+        return projectName.getText();
+    }
+
+    public String getAppJsonPath() {
+        return appJsonPath.getText();
+    }
+
+    public boolean isAppJsonPathCheck() {
+        return appJsonPathCheck.isSelected();
     }
 
     // property view
